1d27244af2ea3a34df84dbe9f775b28ea5973af7
Jhonattan Lino <jhonattanlino@outlook.com>
2022-05-15 14:44:32 -0300

    Update TLC.R

--- TLC.R
+++ TLC.R
@@ -35,7 +35,7 @@ sim %>%
   scale_colour_hc()
 
 sim %>%
-  ggplot(aes(x = uniforme))+ # distribuição uniforme
+  ggplot(aes(x = fFisher))+ # distribuição uniforme
   geom_histogram(aes(y = ..density..),bins = 50, fill = 'black', alpha = 0.8)+
   geom_density(size = 1.5, alpha = 0.9, color = 'red')+
   theme_hc()+
@@ -45,26 +45,33 @@ sim %>%
 
 #### Primeiramente é necessário normalizar os dados
 
-teste <- tibble(indice = 3:n,
+testes <- tibble(indice = 3:n,
                 Pexpo = double(length= n-2),
                 Puni = double(length = n-2),
                 PtStu = double(length = n-2),
                 PfFis = double(length = n-2))
 
-for(i in 3:row(testes)) {
+for(i in 3:nrow(teste)) {
+  janela <- sim %>% 
+    filter(indice <= i) %>% # selecionamos apenas os �ndices at� o atual
+    transmute(uniforme = (uniforme - mean(uniforme))/sd(uniforme),
+              tStudent = (tStudent - mean(tStudent))/sd(tStudent),
+              fFisher = (fFisher - mean(fFisher))/sd(fFisher),
+              exponencial = (exponencial - mean(exponencial))/sd(exponencial))
   
-  janela <- sim %>%
-    filter(indice <= i) %>%
-    transmute(exponencial = (exponencial - mean(exponencial)/sd(exponencial)),
-              uniforme = (uniforme - mean(uniforme)/sd(uniforme)),
-              tStudent = (tStudent - mean(tStudent)/sd(uniforme)),
-              fFisher = (fFisher - mean(fFisher))/sd(uniforme))
-
-  teste$Pexpo[i] <- shapiro.test(janela$exponencial)$p.value
-  teste$Puni[i] <- shapiro.test(janela$uniforme)$p.value
-  teste$PtStu[i] <- shapiro.test(janela$tStudent)$p.value
-  teste$PfFis[i] <- shapiro.test(janela$fFisher)$p.value
+  
+  testes$Puni[i] <- ks.test(x = janela$uniforme, "pnorm")$p.value
+  testes$PtStu[i] <- ks.test(x = janela$tStudent, "pnorm")$p.value
+  testes$PfFis[i] <- ks.test(x = janela$fFisher, "pnorm")$p.value
+  testes$Pexpo[i] <- ks.test(x = janela$exponencial, "pnorm")$p.value
 }
 
-print(test$Pexpo)
 
+testes %>%
+  pivot_longer(Pexpo:PfFis,
+               names_to = "distro",
+               values_to = "p") %>%
+  ggplot(aes(x = indice, color = distro, y = p))+
+  geom_line(size=1.5, alpha = 0.8)+
+  theme_minimal() +
+  scale_y_continuous(label = scales::percent)
